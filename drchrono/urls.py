b3c77547e4ab498146f6e39115e9801d69f5e79a
Noëlle Anthony <noelle.d.anthony@gmail.com>
2017-07-10 17:57:16 -0400

    Got data streaming from external site

--- drchrono/urls.py
+++ drchrono/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
 
-import views
+from views import get_users
 
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
 
     url(r'', include('social.apps.django_app.urls', namespace='social')),
+
+    url(r'^accounts/profile/', get_users)
 ]
+
+# url(r'^complete/drchrono/', TemplateView.as_view(template_name='auth.html'), name='auth'),
--- drchrono/views.py
+++ drchrono/views.py
@@ -1,1 +1,20 @@
 # Create your views here.
+from django.shortcuts import render
+import requests, ast
+from django.db import connection
+
+BASE_URL = "https://drchrono.com"
+
+def get_users(request):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT id, extra_data from social_auth_usersocialauth")
+        row = cursor.fetchone()
+    token = ast.literal_eval(row[1])['access_token']
+    r = requests.get(BASE_URL + '/api/patients_summary', headers={
+        'Authorization': 'Bearer %s' % token,
+    }).json()
+    return render(request, 'auth.html',
+        {'response': r,
+         'row': row,
+         'token': token,
+        })
